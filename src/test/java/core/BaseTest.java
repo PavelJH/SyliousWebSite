ba4194a9b9985d95b5b76e8b0b5df8f4d08981d4
PavelJH <kelbaspavel@gmail.com>
2025-03-14 12:11:23 +0100

    Added windowMaximize and gradlew file

--- src/test/java/core/BaseTest.java
+++ src/test/java/core/BaseTest.java
@@ -2,6 +2,7 @@ package core;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.WebDriverRunner;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import io.qameta.allure.selenide.AllureSelenide;
@@ -9,13 +10,15 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 
+
 public class BaseTest {
 
     public void setUp(){
         WebDriverManager.chromedriver().setup();
         Configuration.browser = "chrome";
         Configuration.webdriverLogsEnabled = true;
-        Configuration.browserSize = "1920x1080";
+        //Configuration.browserSize = "1920x1080";
+        WebDriverRunner.getWebDriver().manage().window().maximize();
         Configuration.headless = true;
     }
 
