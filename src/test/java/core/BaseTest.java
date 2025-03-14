27dfd3ed8b2fceb580a15586c79acca920e1aa3e
PavelJH <kelbaspavel@gmail.com>
2025-03-14 12:32:52 +0100

    Added new BrowsersSize

--- src/test/java/core/BaseTest.java
+++ src/test/java/core/BaseTest.java
@@ -2,7 +2,6 @@ package core;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
-import com.codeborne.selenide.WebDriverRunner;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import io.qameta.allure.selenide.AllureSelenide;
@@ -10,15 +9,14 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 
-
 public class BaseTest {
 
     public void setUp(){
         WebDriverManager.chromedriver().setup();
         Configuration.browser = "chrome";
         Configuration.webdriverLogsEnabled = true;
-        //Configuration.browserSize = "1920x1080";
-        WebDriverRunner.getWebDriver().manage().window().maximize();
+        Configuration.browserSize = "2560x1440";
+//        Configuration.browserSize = "1920x1080";
         Configuration.headless = true;
     }
 
